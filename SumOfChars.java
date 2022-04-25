8f9ff07269ab65b3c150793eb51be26e3ef15a7c
Dayana Stoynova <102184400+DayanaStoynova@users.noreply.github.com>
2022-04-25 09:40:50 +0300

    Add files via upload

---
+++ SumOfChars.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfChars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfLines = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = 1; i <=  numberOfLines; i++) {
+            String letter = scanner.nextLine();
+            int number = letter.charAt(0);
+            sum+=number;
+
+        }
+        System.out.printf("The sum equals: %d",sum);
+    }
+}
